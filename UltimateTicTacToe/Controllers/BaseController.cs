3f7e983150a3f14fb2ce6a112308d0e79ddcc31b
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2018-12-07 16:16:32 +0000

    added base controller to control the error handling that all controllers must have

---
+++ UltimateTicTacToe/Controllers/BaseController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace Controllers
+{
+    [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+    public class BaseController : Controller
+    {
+
+        public BaseController()
+        {
+        }
+
+        protected IActionResult ExecuteApiAction<T>(Func<ApiResult<T>> function, string uri = "")
+        {
+            try
+            {
+                var result = function();
+
+                if (result.ValidationErrors.Any())
+                    return BadRequest(result.ValidationErrors);
+
+                if (result.Model == null)
+                {
+                    return NotFound();
+                }
+
+                if (String.Equals(HttpContext.Request.Method, HttpMethod.Post.ToString(), StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return Created(uri, result.Model);
+                }
+                return Ok(result.Model);
+            }
+            catch (Exception e)
+            {
+                throw; //will return a 500 internal server error
+            }
+        }
+    }
+}
