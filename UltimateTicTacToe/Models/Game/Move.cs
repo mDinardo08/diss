5fb2db76819ac3d75b92848e936b71d1528216a7
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2018-12-07 20:54:45 +0000

    added move object

---
+++ UltimateTicTacToe/Models/Game/Move.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltimateTicTacToe.Models.Game
+{
+    public class Move
+    {
+        public Move next;
+        public System.Drawing.Point move;
+        public Player owner;
+    }
+}
