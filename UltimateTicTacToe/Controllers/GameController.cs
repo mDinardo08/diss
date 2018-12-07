1ab869653be8306b4da655c0efdb423ee5662258
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2018-12-07 17:05:35 +0000

    added game interface

--- UltimateTicTacToe/Controllers/GameController.cs
+++ UltimateTicTacToe/Controllers/GameController.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UltimateTicTacToe.Models;
 
 namespace UltimateTicTacToe.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GameController : ControllerBase
+    public class GameController : BaseController
     {
+
+        [HttpPost("makeMove")]
+        public IActionResult makeMove([FromBody]Game game)
+        {
+            return null;
+        }
     }
 }
