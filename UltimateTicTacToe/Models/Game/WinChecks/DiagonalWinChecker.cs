83f5889b68017f6bd26add5f9fb88d2e83f0892b
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2018-12-09 23:19:41 +0000

    clean up namespace

--- UltimateTicTacToe/Models/Game/WinChecks/DiagonalWinChecker.cs
+++ UltimateTicTacToe/Models/Game/WinChecks/DiagonalWinChecker.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UltimateTicTacToe.Models.Game
+namespace UltimateTicTacToe.Models.Game.WinCheck
 {
     public class DiagonalWinChecker: WinCheckHandler
     { 
