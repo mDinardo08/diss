845b94b73e59a28467fe3ddd7680b526fd8ca13a
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2018-12-16 18:57:24 +0000

    added json converter for win checker

--- UltimateTicTacToe/Models/Game/TicTacToe.cs
+++ UltimateTicTacToe/Models/Game/TicTacToe.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
+using UltimateTicTacToe.JsonConverters.WinCheck;
 using UltimateTicTacToe.Models.Game.WinCheck;
 
 namespace UltimateTicTacToe.Models.Game
@@ -10,6 +12,7 @@ namespace UltimateTicTacToe.Models.Game
     public class TicTacToe : CompositeGame
     {
         public List<List<BoardGame>> board;
+        [JsonConverter(typeof(WinCheckConverter))]
         public IWinChecker winChecker;
 
         public TicTacToe(IWinChecker winChecker)
