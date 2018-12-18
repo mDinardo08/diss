f7a2896e6eefb5e3bf6c3c74e90b847e757fa74b
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2018-12-18 13:09:41 +0000

    removed unused JsonConvert decorator

--- UltimateTicTacToe/Models/DTOs/MoveDTO.cs
+++ UltimateTicTacToe/Models/DTOs/MoveDTO.cs
@@ -6,7 +6,6 @@ namespace UltimateTicTacToe.Models.DTOs
 {
     public class MoveDTO
     {
-        [JsonConverter(typeof(BoardConverter))]
         public BoardGame game;
         public Move move;
     }
