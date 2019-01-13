dead860c30c3416302c69174305a5d24c79a1d5b
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-13 22:34:46 +0000

    api service now gets and posts

--- UltimateTicTacToe/ClientApp/src/app/services/api/api.service.tests.spec.ts
+++ UltimateTicTacToe/ClientApp/src/app/services/api/api.service.tests.spec.ts
@@ -1,5 +1,5 @@
 import { ApiService } from "./api.service";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 describe("Api Service", () => {
 
@@ -24,10 +24,74 @@ describe("Api Service", () => {
 
     it("Will return the object returned from the http client", () => {
         const mockHttp = jasmine.createSpyObj("HttpClient", ["get"]);
-        const observable = new Observable();
+        const observable = new Observable<string>();
         mockHttp.get.and.returnValue(observable);
         service = new ApiService(mockHttp);
         const result = service.get<string>("end");
         expect(result).toBe(observable);
+    });
+
+    it("Will return an object with a httpOptions field", () => {
+        const result = service.getOptions();
+        expect(result.headers).toBeDefined();
+    });
+
+    it("Will have a httpHeaders object in the httpOptions field", () => {
+        const result = service.getOptions();
+        expect(result.headers instanceof HttpHeaders).toBe(true);
+    });
+
+    it("Will set content type to application json", () => {
+        const result = service.getOptions();
+        const options = <HttpHeaders>result.headers;
+        expect(options.get("Content-Type")).toBe("application/json");
+    });
+
+    it("Will call to post at the correct URL", () => {
+        const mockHttp = jasmine.createSpyObj("HttpClient", ["post"]);
+        service = new ApiService(mockHttp);
+        spyOn(service, "getURL").and.returnValue("url");
+        spyOn(service, "getOptions").and.returnValue(null);
+        service.post("/endpoint", null);
+        expect(mockHttp.post).toHaveBeenCalledWith("url/endpoint", null, null);
+    });
+
+    it("Will call to post with the correct the http options", () => {
+        const mockHttp = jasmine.createSpyObj("HttpClient", ["post"]);
+        const options = {
+            value:  {
+                "stuff": "thigs"
+            }
+        };
+        service = new ApiService(mockHttp);
+        spyOn(service, "getURL").and.returnValue("");
+        spyOn(service, "getOptions").and.returnValue(options);
+        service.post("", null);
+        expect(mockHttp.post).toHaveBeenCalledWith("", null, options);
+    });
+
+    it("Will call to post with correct data", () => {
+        const mockHttp = jasmine.createSpyObj("HttpClient", ["post"]);
+        service = new ApiService(mockHttp);
+        spyOn(service, "getURL").and.returnValue("");
+        spyOn(service, "getOptions").and.returnValue(null);
+        const data = {
+            value:  {
+                "stuff": "thigs"
+            }
+        };
+        service.post("", data);
+        expect(mockHttp.post).toHaveBeenCalledWith("", data, null);
+    });
+
+    it("Will return whatever the HttpClient returns", () => {
+        const mockHttp = jasmine.createSpyObj("HttpClient", ["post"]);
+        const expected = new Observable<string>();
+        mockHttp.post.and.returnValue(expected);
+        service = new ApiService(mockHttp);
+        spyOn(service, "getURL").and.returnValue("");
+        spyOn(service, "getOptions").and.returnValue(null);
+        const actual = service.post("", null);
+        expect(actual).toBe(expected);
     });
 });
