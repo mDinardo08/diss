48be49293e3ac4e480d4fa732c147c2799b72450
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-17 23:04:39 +0000

    moveEvent now is of type move

--- UltimateTicTacToe/ClientApp/src/app/components/abstractBoardGame/abstractBoardGame.component.ts
+++ UltimateTicTacToe/ClientApp/src/app/components/abstractBoardGame/abstractBoardGame.component.ts
@@ -1,12 +1,13 @@
 import { Player } from "../../models/player/player.model";
 import { Input, Output, EventEmitter } from "@angular/core";
 import { BoardGame } from "../../models/boardGame/boardgame/boardgame.model";
+import { Move } from "../../models/move/move.model";
 
 export abstract class AbstractBoardGameComponent  {
 
     @Input() owner: Player;
     @Input() board: Array<Array<BoardGame>>;
-    @Output() moveEvent =  new EventEmitter();
+    @Output() moveEvent =  new EventEmitter<Move>();
 
 
 }
