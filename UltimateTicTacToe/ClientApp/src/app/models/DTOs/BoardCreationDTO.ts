7e418158a314e8f5f46b8a2b57c1a0ebfbf095da
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-18 22:53:05 +0000

    added dtos

---
+++ UltimateTicTacToe/ClientApp/src/app/models/DTOs/BoardCreationDTO.ts
@@ -0,0 +1,7 @@
+import { Player } from "../player/player.model";
+
+export class BoardCreationDTO {
+
+    public size: number;
+    public players: Array<Player>;
+}
--- UltimateTicTacToe/ClientApp/src/app/models/DTOs/BoardGameDTO.ts
+++ UltimateTicTacToe/ClientApp/src/app/models/DTOs/BoardGameDTO.ts
@@ -5,6 +5,7 @@ import { BoardGame } from "../boardGame/boardgame/boardgame.model";
 export class BoardGameDTO {
     public game: Array<Array<BoardGame>>;
     public Winner: Player;
-    public next: number;
+    public cur: Player;
+    public players: Array<Player>;
     public lastMove: Move;
 }
