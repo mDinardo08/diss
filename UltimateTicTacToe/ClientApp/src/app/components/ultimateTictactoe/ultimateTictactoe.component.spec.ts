69769c509fc6163c1d071ba8e78ce559ba27a033
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-20 14:27:03 +0000

    ultimate tic tac toe now emits correct event

--- UltimateTicTacToe/ClientApp/src/app/components/ultimateTictactoe/ultimateTictactoe.component.spec.ts
+++ UltimateTicTacToe/ClientApp/src/app/components/ultimateTictactoe/ultimateTictactoe.component.spec.ts
@@ -1,4 +1,5 @@
 import { TictactoeComponent } from "./ultimateTictactoe.component";
+import { Move } from "../../models/move/move.model";
 
 describe("Tictactoe component", () => {
 
@@ -6,5 +7,49 @@ describe("Tictactoe component", () => {
 
     beforeEach(() => {
         comp = new TictactoeComponent();
+    });
+
+    it("Will emit a move event when it recieves one", () => {
+        spyOn(comp.moveEvent, "emit");
+        comp.moveMade(null, null, null);
+        expect(comp.moveEvent.emit).toHaveBeenCalled();
+    });
+
+    it("Will emit a move object", () => {
+        spyOn(comp.moveEvent, "emit");
+        comp.moveMade(null, null, null);
+        expect(comp.moveEvent.emit).toHaveBeenCalledWith(jasmine.any(Move));
+    });
+
+    it("Will emit a move whoes next property is the event it recieved", () => {
+        spyOn(comp.moveEvent, "emit");
+        const move = new Move();
+        comp.moveMade(move, null, null);
+        const event = new Move();
+        event.next = move;
+        event.possition = {
+            X: null, Y: null
+        };
+        expect(comp.moveEvent.emit).toHaveBeenCalledWith(event);
+    });
+
+    it("Will assign a possition with the x being equal to the x passed in", ()  => {
+        spyOn(comp.moveEvent, "emit");
+        const move = new Move();
+        comp.moveMade(null, 0, null);
+        const event = new Move();
+        event.possition = {X: 0, Y: null};
+        event.next = null;
+        expect(comp.moveEvent.emit).toHaveBeenCalledWith(event);
+    });
+
+    it("Will assign a possition with the y being equal to the y passed in", ()  => {
+        spyOn(comp.moveEvent, "emit");
+        const move = new Move();
+        comp.moveMade(null, null, 0);
+        const event = new Move();
+        event.possition = {X: null, Y: 0};
+        event.next = null;
+        expect(comp.moveEvent.emit).toHaveBeenCalledWith(event);
     });
 });
--- UltimateTicTacToe/ClientApp/src/app/components/ultimateTictactoe/ultimateTictactoe.component.ts
+++ UltimateTicTacToe/ClientApp/src/app/components/ultimateTictactoe/ultimateTictactoe.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from "@angular/core";
 import { AbstractBoardGameComponent } from "../abstractBoardGame/abstractBoardGame.component";
 import { BoardGame } from "../../models/boardGame/boardgame/boardgame.model";
 import { Player } from "../../models/player/player.model";
+import { Move } from "../../models/move/move.model";
 
 @Component({
     selector: "ultimateTictactoe",
@@ -10,8 +11,13 @@ import { Player } from "../../models/player/player.model";
 
 export class TictactoeComponent extends AbstractBoardGameComponent {
 
-    moveMade($event: any) {
-        console.log($event);
-        this.moveEvent.emit();
+    moveMade($event: Move, x: number, y: number) {
+        const move = new Move();
+        move.next = $event;
+        move.possition = {
+            X: x,
+            Y: y
+        };
+        this.moveEvent.emit(move);
     }
 }
