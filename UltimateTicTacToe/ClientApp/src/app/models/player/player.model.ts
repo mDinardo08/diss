9db64589c64528de20fe447dec923b504ddb6828
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-21 18:27:43 +0000

    added player colours

--- UltimateTicTacToe/ClientApp/src/app/models/player/player.model.ts
+++ UltimateTicTacToe/ClientApp/src/app/models/player/player.model.ts
@@ -1,7 +1,9 @@
 import { PlayerType } from "./player.type.enum";
+import { PlayerColour } from "./player.colour.enum";
 
 export class Player {
 
     public type: PlayerType;
     public name: string;
+    public colour: PlayerColour;
 }
