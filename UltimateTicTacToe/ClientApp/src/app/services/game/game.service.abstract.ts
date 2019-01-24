ee2104d37fb6434ce0949351371b95c24a367a60
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-24 15:31:40 +0000

    game service now is typed

--- UltimateTicTacToe/ClientApp/src/app/services/game/game.service.abstract.ts
+++ UltimateTicTacToe/ClientApp/src/app/services/game/game.service.abstract.ts
@@ -1,11 +1,13 @@
 import { Move } from "../../models/move/move.model";
 import { Player } from "../../models/player/player.model";
+import { Observable } from "rxjs/Observable";
+import { BoardGameDTO } from "../../models/DTOs/BoardGameDTO";
 
 export abstract class AbstractGameService {
 
-    abstract makeMove(move: Move);
+    abstract makeMove(move: Move): Observable<BoardGameDTO>;
 
-    abstract createGame(size: number, players: Array<Player>);
+    abstract createGame(size: number, players: Array<Player>): Observable<BoardGameDTO>;
 
-    abstract getCurrentPlayer();
+    abstract getCurrentPlayer(): Player;
 }
