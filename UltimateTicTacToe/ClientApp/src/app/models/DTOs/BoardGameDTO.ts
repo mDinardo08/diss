7505fc37c8a063d786470c9aa946684d50ae2ce6
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-27 19:23:46 +0000

    made dtos agree

--- UltimateTicTacToe/ClientApp/src/app/models/DTOs/BoardGameDTO.ts
+++ UltimateTicTacToe/ClientApp/src/app/models/DTOs/BoardGameDTO.ts
@@ -4,7 +4,7 @@ import { BoardGame } from "../boardGame/boardgame/boardgame.model";
 
 export class BoardGameDTO {
     public game: Array<Array<BoardGame>>;
-    public Winner: Player;
+    public winner: Player;
     public cur: Player;
     public players: Array<Player>;
     public lastMove: Move;
