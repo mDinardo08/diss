1479fa1a3a1b2ea83e0bb5491221d3f14df7a0f6
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-28 21:35:38 +0000

    added setup component to ng module

--- UltimateTicTacToe/ClientApp/src/app/app.module.ts
+++ UltimateTicTacToe/ClientApp/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { RouterModule } from "@angular/router";
 import * as Components from "./components";
 import { AppComponent } from "./app.component";
 import * as Services from "./services";
+import { ModalModule } from "ngx-bootstrap/modal";
 @NgModule({
   declarations: [
     AppComponent,
     Components.NavMenuComponent,
     Components.GameComponent,
     Components.TictactoeComponent,
-    Components.TileComponent
+    Components.TileComponent,
+    Components.GameSetupComponent
   ],
   imports: [
+    ModalModule.forRoot(),
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
@@ -22,6 +25,7 @@ import * as Services from "./services";
       { path: "", component: Components.GameComponent, pathMatch: "full" }
     ])
   ],
+  entryComponents: [Components.GameSetupComponent],
   providers: [Services.ApiService, { provide: Services.AbstractGameService, useClass: Services.GameService }],
   bootstrap: [AppComponent]
 })
--- UltimateTicTacToe/ClientApp/src/app/components/index.ts
+++ UltimateTicTacToe/ClientApp/src/app/components/index.ts
@@ -2,3 +2,4 @@ export * from "./game/game.component";
 export * from "./nav-menu/nav-menu.component";
 export * from "./ultimateTictactoe/ultimateTictactoe.component";
 export * from "./tile/tile.component";
+export * from "./gameSetup/setup.component";
