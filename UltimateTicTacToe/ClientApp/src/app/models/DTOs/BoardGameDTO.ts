9fa979080c68db8b88bfb54b610bfe2dc99902a1
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-31 15:45:44 +0000

    changed winner to player colour

--- UltimateTicTacToe/ClientApp/src/app/models/DTOs/BoardGameDTO.ts
+++ UltimateTicTacToe/ClientApp/src/app/models/DTOs/BoardGameDTO.ts
@@ -1,10 +1,11 @@
 import { Player } from "../player/player.model";
 import { Move } from "../move/move.model";
 import { BoardGame } from "../boardGame/boardgame/boardgame.model";
+import { PlayerColour } from "../player/player.colour.enum";
 
 export class BoardGameDTO {
     public game: Array<Array<BoardGame>>;
-    public winner: Player;
+    public winner: PlayerColour;
     public cur: Player;
     public players: Array<Player>;
     public lastMove: Move;
