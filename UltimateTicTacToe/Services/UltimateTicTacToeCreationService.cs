bb0fa87c0925909887d33a92eabae7d4d139f946
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-01-31 11:50:24 +0000

    board creation now populates owner as player colour

--- UltimateTicTacToe/Services/UltimateTicTacToeCreationService.cs
+++ UltimateTicTacToe/Services/UltimateTicTacToeCreationService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using UltimateTicTacToe.Models.DTOs;
 using UltimateTicTacToe.Models.Game;
+using UltimateTicTacToe.Models.Game.Players;
 using UltimateTicTacToe.Models.Game.WinCheck;
 
 namespace UltimateTicTacToe.Services
@@ -76,7 +77,7 @@ namespace UltimateTicTacToe.Services
                     else
                     {
                         TicTacToe innerGame = createTicTacToe(space["board"].ToObject<List<List<JObject>>>());
-                        innerGame.owner = playerCreationService.createPlayer(space["owner"] as JObject);
+                        innerGame.owner = space["owner"].ToObject<PlayerColour?>();
                         board[row].Add(innerGame);
                     }
                 }
@@ -88,7 +89,7 @@ namespace UltimateTicTacToe.Services
         private Tile createTile(JObject jObject)
         {
             Tile t = new Tile();
-            t.owner = playerCreationService.createPlayer(jObject["owner"] as JObject);
+            t.owner = jObject["owner"].ToObject<PlayerColour?>();
             return t;
         }
     }
