60e76383a8c39c974f479613f3131293b1a45846
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-02-03 21:58:22 +0000

    added move dto

---
+++ UltimateTicTacToe/Models/DTOs/MoveDto.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltimateTicTacToe.Models.Game;
+
+namespace UltimateTicTacToe.Models.DTOs
+{
+    public class MoveDto
+    {
+        Move move;
+        public List<List<JObject>> game;
+    }
+}
