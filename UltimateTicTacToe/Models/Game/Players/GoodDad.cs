3625190db9c7706fbcabbe9ddcba5aff7a16a2b2
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-02-09 22:45:45 +0000

    good dad complete

---
+++ UltimateTicTacToe/Models/Game/Players/GoodDad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltimateTicTacToe.DataAccess;
+using UltimateTicTacToe.Models.DTOs;
+using UltimateTicTacToe.Models.MCTS;
+using UltimateTicTacToe.Services;
+
+namespace UltimateTicTacToe.Models.Game.Players
+{
+    public class GoodDad : AbstractPlayer
+    {
+        public GoodDad(IRandomService random, IDatabaseProvider provider) : base(random, provider)
+        {
+        }
+
+        protected override INode decideMove(BoardGame game, List<INode> nodes, RatingDTO opponentRating)
+        {
+            double diff = int.MaxValue;
+            INode best = nodes[0];
+            double targetScore = 0.6 * opponentRating.average + 0.4 * opponentRating.latest;
+            foreach (INode node in nodes)
+            {
+                double nodeDiff = Math.Abs(targetScore - node.getReward());
+                if (nodeDiff < diff)
+                {
+                    diff = nodeDiff;
+                    best = node;
+                }
+            }
+            return best; 
+        }
+    }
+}
---
+++ UltimateTicTacToeTests/Models/GoodDadTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UltimateTicTacToe.DataAccess;
+using UltimateTicTacToe.Models.DTOs;
+using UltimateTicTacToe.Models.Game;
+using UltimateTicTacToe.Models.Game.Players;
+using UltimateTicTacToe.Models.MCTS;
+
+namespace UltimateTicTacToeTests.Models
+{
+    [TestClass]
+    public class GoodDadTests
+    {
+        Player player;
+        [TestInitialize()]
+        public void Setup()
+        {
+            Mock<IDatabaseProvider> mockProvider = new Mock<IDatabaseProvider>();
+            mockProvider.Setup(x => x.getUser(It.IsAny<int>())).Returns(new RatingDTO());
+            player = new GoodDad(null, mockProvider.Object);
+        }
+
+        [TestMethod]
+        public void WillCallForRewardFromNode()
+        {
+            Mock<INode> node = new Mock<INode>();
+            node.Setup(x => x.getReward())
+                .Returns(0)
+                .Verifiable();
+            node.Setup(x => x.getMove())
+                .Returns(new Move());
+            player.makeMove(null, new List<INode>
+            {
+                node.Object
+            }, 0);
+            node.Verify();
+        }
+
+        [TestMethod]
+        public void WillCallAllNodesInList()
+        {
+            Mock<INode> node = new Mock<INode>();
+            node.Setup(x => x.getReward())
+                .Returns(0)
+                .Verifiable();
+            node.Setup(x => x.getMove())
+                .Returns(new Move());
+            player.makeMove(null, new List<INode>
+            {
+                node.Object, node.Object
+            }, 0);
+            node.Verify(x => x.getReward(), Times.Exactly(2));
+        }
+
+        [TestMethod]
+        public void WillReturnANodeWhichRewardIsClosestTheDTOAverageValueIfLatestIsZero()
+        {
+            Mock<IDatabaseProvider> mockProvider = new Mock<IDatabaseProvider>();
+            mockProvider.Setup(x => x.getUser(It.IsAny<int>()))
+            .Returns(new RatingDTO { average = 1, latest = 0 });
+            player = new GoodDad(null, mockProvider.Object);
+            Mock<INode> node = new Mock<INode>();
+            node.Setup(x => x.getReward())
+                .Returns(0.9);
+            node.Setup(x => x.getMove())
+                .Returns(new Move());
+            Mock<INode> badNode = new Mock<INode>();
+            badNode.Setup(x => x.getReward()).Returns(-1);
+            INode result = player.makeMove(null, new List<INode>
+            {
+                badNode.Object, node.Object
+            }, 0);
+            Assert.AreSame(node.Object, result);
+        }
+
+        [TestMethod]
+        public void WillReturnANodeWhichRewardIs60PercentAverageAnd40PercentLatest()
+        {
+            Mock<IDatabaseProvider> mockProvider = new Mock<IDatabaseProvider>();
+            mockProvider.Setup(x => x.getUser(It.IsAny<int>()))
+            .Returns(new RatingDTO { average = 0.5, latest = 1 });
+            player = new GoodDad(null, mockProvider.Object);
+            Mock<INode> node = new Mock<INode>();
+            node.Setup(x => x.getReward())
+                .Returns(0.7);
+            node.Setup(x => x.getMove())
+                .Returns(new Move());
+            Mock<INode> badNode = new Mock<INode>();
+            badNode.Setup(x => x.getReward()).Returns(0.5);
+            INode result = player.makeMove(null, new List<INode>
+            {
+                node.Object, badNode.Object
+            }, 0);
+            Assert.AreSame(node.Object, result);
+        }
+    }
+}
