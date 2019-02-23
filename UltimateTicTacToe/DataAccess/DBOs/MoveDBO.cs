ef84c917b8bc734f620e3e780084249c936fc852
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-02-23 23:28:26 +0000

    added move dbo

---
+++ UltimateTicTacToe/DataAccess/DBOs/MoveDBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltimateTicTacToe.DataAccess.DBOs
+{
+    public class MoveDBO
+    {
+        public int Owner;
+        public double reward;
+        public int place;
+    }
+}
