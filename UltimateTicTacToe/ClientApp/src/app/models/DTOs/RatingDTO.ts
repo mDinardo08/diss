a77a9c62a0d9a5c41427a9102f9b422292387f56
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2019-02-23 23:29:09 +0000

    added more fields to Rating dtos

--- UltimateTicTacToe/ClientApp/src/app/models/DTOs/RatingDTO.ts
+++ UltimateTicTacToe/ClientApp/src/app/models/DTOs/RatingDTO.ts
@@ -3,4 +3,6 @@ export class RatingDTO {
     public userId: number;
     public latest: number;
     public average: number;
+    public highOption: number;
+    public lowOption: number;
 }
--- UltimateTicTacToe/Models/DTOs/RatingDto.cs
+++ UltimateTicTacToe/Models/DTOs/RatingDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UltimateTicTacToe.DataAccess.DBOs;
 
 namespace UltimateTicTacToe.Models.DTOs
 {
@@ -9,6 +10,9 @@ namespace UltimateTicTacToe.Models.DTOs
     {
         public int userId;
         public double latest;
+        public double highOption;
+        public double lowOption;
         public double average;
+        public List<MoveDBO> moves;
     }
 }
